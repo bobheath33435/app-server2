5a130584d6dde62a598866aeddf2c8d71b466cb1
Robert Heath <bob.heath@aspnet.net>
2019-09-23 14:36:03 -0400

    Add more tests for /register route.

--- test/userRouter.test.js
+++ test/userRouter.test.js
@@ -28,12 +28,15 @@ describe('Test UserRouter', () => {
     describe('Test UserRouter', () => {
         beforeEach(async() => {
             const userModel = modelMap[userKey]
-            const response = await userModel.deleteMany({userName: "wmatt", email: "ward@xxx.com"})
+            // var response = await userModel.deleteMany({})
+            var response = await userModel.deleteMany({userName: "wmatt", email: "ward@xxx.com"})
+            logger.trace(`response: ${JSON.stringify(response)}`);
+            response = await userModel.deleteMany({userName: "bfife", email: "bfife@xxx.com"})
             logger.trace(`response: ${JSON.stringify(response)}`);
             log4js.configure({
                 // appenders: { h1bData: { type: 'file', filename: 'h1bData.log' } },
                 appenders: { h1bData: { type: 'console'} },
-                categories: { default: { appenders: ['h1bData'], level: 'off' } }
+                categories: { default: { appenders: ['h1bData'], level: 'FATAL' } }
             });
         })
         afterEach(() => {
@@ -119,6 +122,93 @@ describe('Test UserRouter', () => {
             response = await request(app).post('/register').send(body).expect(201)
             logger.trace(chalk.bgRed.white.bold(`response: ${JSON.stringify(response.text)}`))
             expect(userRouter.NEW_USER_CREATED).to.be.equal(response.text)
+        })
+        it('8) Testing /register with valid data and then try to register again', async () => {
+            var clientData = {}
+            clientData[firstName] = "Ward"
+            clientData[lastName] = "Bond"
+            clientData[email] = "ward@xxx.com"
+            clientData[userName] = "wmatt"
+            clientData[password] = "password"
+            var body = { clientData }
+            var response = await request(app).post('/register').send(body).expect(201)
+            logger.trace(chalk.bgRed.white.bold(`response: ${JSON.stringify(response.text)}`))
+            expect(userRouter.NEW_USER_CREATED).to.be.equal(response.text)
+
+            response = await request(app).post('/register').send(body).expect(500)
+            logger.trace(chalk.bgRed.white.bold(`response: ${JSON.stringify(response.text)}`))
+            expect(userRouter.INVALID_REQUEST).to.be.equal(response.text)
+        })
+        it('9) Testing /register register and try to register again using same email address', async () => {
+            var clientData = {}
+            clientData[firstName] = "Ward"
+            clientData[lastName] = "Bond"
+            clientData[email] = "ward@xxx.com"
+            clientData[userName] = "wmatt"
+            clientData[password] = "password"
+            var body = { clientData }
+            var response = await request(app).post('/register').send(body).expect(201)
+            logger.trace(chalk.bgRed.white.bold(`response: ${JSON.stringify(response.text)}`))
+            expect(userRouter.NEW_USER_CREATED).to.be.equal(response.text)
+
+            clientData[firstName] = "Barnie"
+            clientData[lastName] = "Fife"
+            clientData[email] = "ward@xxx.com"
+            clientData[userName] = "bfife"
+            clientData[password] = "passwordxyz"
+            body = { clientData }
+            response = await request(app).post('/register').send(body).expect(500)
+            logger.trace(chalk.bgRed.white.bold(`response: ${JSON.stringify(response.text)}`))
+            expect(userRouter.INVALID_REQUEST).to.be.equal(response.text)
+        })
+        it('10) Testing /register register and try to register again using same userName', async () => {
+            var clientData = {}
+            clientData[firstName] = "Ward"
+            clientData[lastName] = "Bond"
+            clientData[email] = "ward@xxx.com"
+            clientData[userName] = "wmatt"
+            clientData[password] = "password"
+            var body = { clientData }
+            var response = await request(app).post('/register').send(body).expect(201)
+            logger.trace(chalk.bgRed.white.bold(`response: ${JSON.stringify(response.text)}`))
+            expect(userRouter.NEW_USER_CREATED).to.be.equal(response.text)
+
+            clientData = {}
+            clientData[firstName] = "Barnie"
+            clientData[lastName] = "Fife"
+            clientData[email] = "bfife@xxx.com"
+            clientData[userName] = "wmatt"
+            clientData[password] = "passwordxyz"
+            body = { clientData }
+            response = await request(app).post('/register').send(body).expect(500)
+            logger.trace(chalk.bgRed.white.bold(`response: ${JSON.stringify(response.text)}`))
+            expect(userRouter.INVALID_REQUEST).to.be.equal(response.text)
+        })
+        it('11) Testing /register register with valid requests twice', async () => {
+            var clientData = {}
+            clientData[firstName] = "Ward"
+            clientData[lastName] = "Bond"
+            clientData[email] = "ward@xxx.com"
+            clientData[userName] = "wmatt"
+            clientData[password] = "password"
+            var body = { clientData }
+            logger.error(chalk.bgRed.white.bold(`clientData: ${JSON.stringify(clientData)}`))
+            var response = await request(app).post('/register').send(body).expect(201)
+            logger.trace(chalk.bgRed.white.bold(`response: ${JSON.stringify(response.text)}`))
+            expect(userRouter.NEW_USER_CREATED).to.be.equal(response.text)
+
+            clientData = {}
+            clientData[firstName] = "sam"
+            clientData[lastName] = "smith"
+            clientData[email] = "bfife@xxx.com"
+            clientData[userName] = "bfife"
+            clientData[password] = "passwordxyz"
+            body = { clientData }
+            response = await request(app).post('/register').send(body).expect(201)
+            logger.trace(chalk.bgRed.white.bold(`response: ${JSON.stringify(response.text)}`))
+            expect(userRouter.NEW_USER_CREATED).to.be.equal(response.text)
+        })
+        it('12) Empty', async () => {
         })
     })
 })   
